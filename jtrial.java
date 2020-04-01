f93d5738107ed4629739f8f70948e4a3ba1356cd
cicada <cicadawings3302@gmail.com>
2020-04-01 12:56:42 +0530

    commit from rakg

--- jtrial.java
+++ jtrial.java
@@ -2,6 +2,6 @@ class jtrial
 {
     public static void main(String args{})
     {
-        System.out.println("Please work it will you shut the fuck up..!!");
+        System.out.println("Please work it will you please shut the fuck up..!!");
     }
 }
