a363ef79681accc8141b2d004466c1c9cc3b4d9a
vicky2315 <vigs@publicisgroupe.net>
2020-04-01 10:09:20 +0530

    umm

--- jtrial.java
+++ jtrial.java
@@ -2,6 +2,8 @@ class jtrial
 {
     public static void main(String args{})
     {
-        System.out.println("Please work for fuck's sake");
+        System.out.println("Please work");
+	System.out.println("please work version 1");
+	System.out.println("Please work version 2");
     }
 }
